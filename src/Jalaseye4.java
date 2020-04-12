d621e116a615e9627f65b78348021bfba72283d5
stareh <starteh@example.com>
2020-04-12 09:46:12 -0700

    jalaseye4

---
+++ src/Jalaseye4.java
@@ -0,0 +1,42 @@
+/**
+ * Created by star on 4/12/2020.
+ */
+public class Jalaseye4 {
+    public static void main(String[] args) {
+        int i = 3;
+        if (i == 2) {
+            System.out.println("i mosavi  ba 2 ast");
+        } else {
+            System.out.println("i mosavi ba 2 nist");
+        }
+        int j = 10;
+        switch (j) {
+            case 0:
+                System.out.println("shanbe");
+                break;
+            case 1:
+                System.out.println("yekshanbe");
+                break;
+            case 2:
+                System.out.println("doshanbe");
+                break;
+            case 3:
+                System.out.println("seshanbe");
+                break;
+            case 4:
+                System.out.println("4shanbe");
+                break;
+            case 5:
+                System.out.println("5shanbe");
+                break;
+            case 6:
+                System.out.println("jome");
+                break;
+            default:
+                System.out.println("adde vared shode sahih nist");
+        }
+    }
+
+}
+
+
